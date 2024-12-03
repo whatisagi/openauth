f42d6e98998b5b8b1a0ff4e5f563eb4c2e87cfa5
Dax Raad <d@ironbay.co>
2024-12-03 15:17:25 -0500

    more

--- packages/examples/src/bun/authorizer.ts
+++ packages/examples/src/bun/authorizer.ts
@@ -9,9 +9,9 @@ import { TwitchAdapter } from "@openauthjs/core/adapter/twitch";
 import { GithubAdapter } from "@openauthjs/core/adapter/github";
 import { GoogleAdapter } from "@openauthjs/core/adapter/google";
 import { subjects } from "../subjects.js";
-import { Theme, THEME_INCEL } from "@openauthjs/core/ui/theme";
+import { Theme, THEME_VERCEL } from "@openauthjs/core/ui/theme";
 
-const theme = THEME_INCEL;
+const theme = THEME_VERCEL;
 
 export default authorizer({
   subjects,
