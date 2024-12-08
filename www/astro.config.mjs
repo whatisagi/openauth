bf0ca970ba40bdc9a3f58b4280c8910524d61403
Dax Raad <d@ironbay.co>
2024-12-08 17:31:25 -0500

    fix basepath

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -4,43 +4,42 @@ import config from "./config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://openauthjs.github.io/openauth/",
-  base: "openauth",
-	devToolbar: {
-		enabled: false,
-	},
-	integrations: [
-		starlight({
-			title: "OpenAuth",
-			description: "Universal, standards-based auth provider.",
-			head: [
-				{
-					tag: 'link',
-					attrs: {
-						rel: 'icon',
-						href: '/favicon.ico',
-						sizes: '48x48',
-					},
-				},
-			],
-			social: {
-				github: config.github,
-				discord: config.discord,
-			},
-			pagefind: false,
-			components: {
-				Hero: "./src/components/Hero.astro",
-				Head: "./src/components/Head.astro",
-				Footer: "./src/components/Footer.astro",
-			},
-			customCss: [
-				"@fontsource/ibm-plex-mono/400.css",
-				"@fontsource/ibm-plex-mono/400-italic.css",
-				"@fontsource/ibm-plex-mono/500.css",
-				"@fontsource/ibm-plex-mono/600.css",
-				"@fontsource/ibm-plex-mono/700.css",
-				"./src/custom.css",
-			],
-		}),
-	],
+  site: "https://openauth.js.org",
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [
+    starlight({
+      title: "OpenAuth",
+      description: "Universal, standards-based auth provider.",
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            href: "/favicon.ico",
+            sizes: "48x48",
+          },
+        },
+      ],
+      social: {
+        github: config.github,
+        discord: config.discord,
+      },
+      pagefind: false,
+      components: {
+        Hero: "./src/components/Hero.astro",
+        Head: "./src/components/Head.astro",
+        Footer: "./src/components/Footer.astro",
+      },
+      customCss: [
+        "@fontsource/ibm-plex-mono/400.css",
+        "@fontsource/ibm-plex-mono/400-italic.css",
+        "@fontsource/ibm-plex-mono/500.css",
+        "@fontsource/ibm-plex-mono/600.css",
+        "@fontsource/ibm-plex-mono/700.css",
+        "./src/custom.css",
+      ],
+    }),
+  ],
 });
