031f23ef452219fc8aacf3c1f8b38120d608aa50
Jay V <air@live.ca>
2024-12-09 13:23:53 -0500

    testing

--- examples/authorizer/bun/authorizer.ts
+++ examples/authorizer/bun/authorizer.ts
@@ -2,7 +2,6 @@ import { authorizer } from "@openauthjs/openauth";
 import { MemoryStorage } from "@openauthjs/openauth/storage/memory";
 import { PasswordAdapter } from "@openauthjs/openauth/adapter/password";
 import { PasswordUI } from "@openauthjs/openauth/ui/password";
-import { THEME_TERMINAL, THEME_SST, THEME_VERCEL, THEME_SUPABASE } from "@openauthjs/openauth/ui/theme";
 import { subjects } from "../../subjects.js";
 
 export default authorizer({
@@ -19,7 +18,6 @@ export default authorizer({
       })
     ),
   },
-  theme: THEME_SUPABASE,
   success: async (ctx, value) => {
     if (value.provider === "password") {
       return ctx.subject("user", {
