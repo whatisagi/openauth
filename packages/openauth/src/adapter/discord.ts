249c5cd389634cc9dadfdbd1bc04f413f0265dd1
Pranjal Butola <71776171+fero1xd@users.noreply.github.com>
2024-12-10 21:37:44 +0530

    feat: added discord adapter (#14)

---
+++ packages/openauth/src/adapter/discord.ts
@@ -0,0 +1,12 @@
+import { Oauth2Adapter, Oauth2WrappedConfig } from "./oauth2.js";
+
+export function DiscordAdapter(config: Oauth2WrappedConfig) {
+  return Oauth2Adapter({
+    type: "discord",
+    ...config,
+    endpoint: {
+      authorization: "https://discord.com/oauth2/authorize",
+      token: "https://discord.com/api/oauth2/token",
+    },
+  });
+}
