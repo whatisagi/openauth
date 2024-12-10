7857f855f4c0e65a2a5d15cf87a30aea2833b851
Tathagata Chakraborty <tathagatachakraborty97@gmail.com>
2024-12-11 02:12:56 +0530

    feature: add facebook o-auth (#20)

---
+++ packages/openauth/src/adapter/facebook.ts
@@ -0,0 +1,21 @@
+import { Oauth2Adapter, Oauth2WrappedConfig } from "./oauth2.js";
+import { OidcAdapter, OidcWrappedConfig } from "./oidc.js";
+
+export function FacebookAdapter(config: Oauth2WrappedConfig) {
+  return Oauth2Adapter({
+    ...config,
+    type: "facebook",
+    endpoint: {
+      authorization: "https://www.facebook.com/v12.0/dialog/oauth", 
+      token: "https://graph.facebook.com/v12.0/oauth/access_token", 
+    },
+  });
+}
+
+export function FacebookOidcAdapter(config: OidcWrappedConfig) {
+  return OidcAdapter({
+    ...config,
+    type: "facebook",
+    issuer: "https://graph.facebook.com", 
+  });
+}
