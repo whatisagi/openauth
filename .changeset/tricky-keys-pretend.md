079c51474342243cf01c616c738a84b09fa11da2
AlexGaudon <35076791+AlexGaudon@users.noreply.github.com>
2024-12-11 10:47:13 -0330

    feat: add JumpCloud (#32)
    * feat: add JumpCloud
    
    * Create tricky-keys-pretend.md
    
    ---------
    
    Co-authored-by: Dax <mail@thdxr.com>

---
+++ .changeset/tricky-keys-pretend.md
@@ -0,0 +1,5 @@
+---
+"@openauthjs/openauth": patch
+---
+
+feat: add JumpCloud
---
+++ packages/openauth/src/adapter/jumpcloud.ts
@@ -0,0 +1,12 @@
+import { Oauth2Adapter, Oauth2WrappedConfig } from "./oauth2.js"
+
+export function JumpCloudAdapter(config: Oauth2WrappedConfig) {
+  return Oauth2Adapter({
+    type: "jumpcloud",
+    ...config,
+    endpoint: {
+      authorization: "https://oauth.id.jumpcloud.com/oauth2/auth",
+      token: "https://oauth.id.jumpcloud.com/oauth2/token",
+    },
+  });
+}
