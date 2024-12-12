c8d9a4e7b79cb115516bf52009da88938340c1e5
Jay V <air@live.ca>
2024-12-12 16:43:09 -0500

    examples nextjs

---
+++ examples/client/nextjs/app/api/callback/route.ts
@@ -0,0 +1,17 @@
+import { client, setTokens } from "../../auth";
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const code = url.searchParams.get("code");
+
+  try {
+    const tokens = await client.exchange(code!, `${url.origin}/api/callback`);
+    await setTokens(tokens.access, tokens.refresh);
+
+    return NextResponse.redirect(url.origin + "/");
+  }
+  catch (e) {
+    return NextResponse.json(e, { status: 500 });
+  }
+}
---
+++ examples/client/nextjs/app/page.tsx
@@ -0,0 +1,96 @@
+import Image from "next/image";
+import { auth, login, logout } from "./actions";
+import styles from "./page.module.css";
+
+export default async function Home() {
+  const subject = await auth();
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol>
+          {subject ? (
+            <>
+              <li>
+                Logged in as <code>{subject.properties.email}</code>.
+              </li>
+              <li>
+                And then check out <code>app/page.tsx</code>.
+              </li>
+            </>
+          ) : (
+            <>
+              <li>Login with your email and password.</li>
+              <li>
+                And then check out <code>app/page.tsx</code>.
+              </li>
+            </>
+          )}
+        </ol>
+
+        <div className={styles.ctas}>
+          {subject ? (
+            <form action={logout}>
+              <button className={styles.secondary}>Logout</button>
+            </form>
+          ) : (
+            <form action={login}>
+              <button className={styles.primary}>Login with OpenAuth</button>
+            </form>
+          )}
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
