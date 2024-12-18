f3fe3949684f688190d65f3b756fb8db4979431e
Jay V <air@live.ca>
2024-12-18 15:52:43 -0500

    adding docs theme

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -21,16 +21,40 @@ export default defineConfig({
             sizes: "48x48",
           },
         },
+        // Add light/dark mode favicon
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            href: "/favicon.svg",
+            media: "(prefers-color-scheme: light)",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            href: "/favicon-dark.svg",
+            media: "(prefers-color-scheme: dark)",
+          },
+        },
       ],
+      pagination: false,
+      logo: {
+        light: "./src/assets/logo-light.svg",
+        dark: "./src/assets/logo-dark.svg",
+        replacesTitle: true,
+      },
       social: {
         github: config.github,
         discord: config.discord,
       },
-      pagefind: false,
       components: {
         Hero: "./src/components/Hero.astro",
         Head: "./src/components/Head.astro",
+        Header: "./src/components/Header.astro",
         Footer: "./src/components/Footer.astro",
+        PageTitle: "./src/components/PageTitle.astro",
       },
       customCss: [
         "@fontsource/ibm-plex-mono/400.css",
@@ -39,6 +63,56 @@ export default defineConfig({
         "@fontsource/ibm-plex-mono/600.css",
         "@fontsource/ibm-plex-mono/700.css",
         "./src/custom.css",
+        "./src/styles/tsdoc.css",
+        "./src/styles/lander.css",
+        "./src/styles/markdown.css",
+      ],
+      sidebar: [
+        { label: "Intro", slug: "docs" },
+        {
+          label: "Get Started",
+          items: [
+            { label: "Test Nav 1", slug: "docs" },
+            { label: "Test Nav 2", slug: "docs" },
+            { label: "Test Nav 3", slug: "docs" },
+            { label: "Test Nav 4", slug: "docs" },
+            { label: "Test Nav 5", slug: "docs" },
+            { label: "Test Nav 6", slug: "docs" },
+          ],
+        },
+        {
+          label: "Get Started",
+          items: [
+            { label: "Test Nav 1", slug: "docs" },
+            { label: "Test Nav 2", slug: "docs" },
+            { label: "Test Nav 3", slug: "docs" },
+            { label: "Test Nav 4", slug: "docs" },
+            { label: "Test Nav 5", slug: "docs" },
+            { label: "Test Nav 6", slug: "docs" },
+          ],
+        },
+        {
+          label: "Get Started",
+          items: [
+            { label: "Test Nav 1", slug: "docs" },
+            { label: "Test Nav 2", slug: "docs" },
+            { label: "Test Nav 3", slug: "docs" },
+            { label: "Test Nav 4", slug: "docs" },
+            { label: "Test Nav 5", slug: "docs" },
+            { label: "Test Nav 6", slug: "docs" },
+          ],
+        },
+        {
+          label: "Get Started",
+          items: [
+            { label: "Test Nav 1", slug: "docs" },
+            { label: "Test Nav 2", slug: "docs" },
+            { label: "Test Nav 3", slug: "docs" },
+            { label: "Test Nav 4", slug: "docs" },
+            { label: "Test Nav 5", slug: "docs" },
+            { label: "Test Nav 6", slug: "docs" },
+          ],
+        },
       ],
     }),
   ],
