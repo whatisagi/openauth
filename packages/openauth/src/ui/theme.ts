bf7209a5630456f3c76ef7cc9a070c944c35458f
Jay V <air@live.ca>
2024-12-23 13:51:29 -0500

    fixing safari support for themes

--- packages/openauth/src/ui/theme.ts
+++ packages/openauth/src/ui/theme.ts
@@ -79,7 +79,7 @@ export const THEME_SUPABASE: Theme = {
     family: "Varela Round, sans-serif",
   },
   css: `
-    @import url('https://fonts.googleapis.com/css2?family=Varela Round:wght@100;200;300;400;500;600;700;800;900&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Varela+Round:wght@100;200;300;400;500;600;700;800;900&display=swap');
   `,
 }
 
