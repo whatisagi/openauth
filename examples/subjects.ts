033842806b802248922a698b5431274143895dd6
Dax Raad <d@ironbay.co>
2025-01-03 16:38:21 -0500

    example: fix

--- examples/subjects.ts
+++ examples/subjects.ts
@@ -1,5 +1,5 @@
 import { object, string } from "valibot"
-import { createSubjects } from "@openauthjs/openauth"
+import { createSubjects } from "@openauthjs/openauth/subject"
 
 export const subjects = createSubjects({
   user: object({
