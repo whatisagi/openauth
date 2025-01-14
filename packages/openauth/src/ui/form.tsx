bf002fd05cf8f539219f9cde584c82d7798190f8
Jay V <air@live.ca>
2025-01-14 16:23:49 -0500

    Styles for admonitions

--- packages/openauth/src/ui/form.tsx
+++ packages/openauth/src/ui/form.tsx
@@ -7,13 +7,22 @@ export function FormAlert(props: {
   return (
     <div data-component="form-alert" data-color={props.color}>
       <svg
-        data-slot="icon"
+        data-slot="icon-success"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
         stroke-width="1.5"
         stroke="currentColor"
-        class="size-6"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+      </svg>
+      <svg
+        data-slot="icon-danger"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
       >
         <path
           stroke-linecap="round"
